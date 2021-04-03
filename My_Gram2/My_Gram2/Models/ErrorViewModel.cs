d112ad3510f296d582404693301688d3f093c0f2
Alejandra Berrios <>
2021-04-02 18:02:02 -0600

    Week 1 - second assignment

---
+++ My_Gram2/My_Gram2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace My_Gram2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
